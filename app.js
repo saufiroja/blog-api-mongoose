328733f2d8154463ea4146fa25d59799d683981b
saufiroja <saufiroja08@gmail.com>
2022-01-17 11:14:07 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+// CONNECTION DATABASE
+require('./database/config');
+
+// MIDDLEWARES
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+// HANDLING ERROR
+app.use((err, req, res, next) => {
+  const { message, code = 500, error = 'internal server error' } = err;
+  return res.status(code).json({
+    message,
+    code,
+    error,
+  });
+});
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`connect on port ${PORT}`);
+});
